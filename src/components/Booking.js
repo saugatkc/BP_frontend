cc3810fce710b1bdf110a1fa7423f9aeddca48a6
saugatkc <thelastresort35@gmail.com>
2020-02-18 11:49:31 +0545

    bookings fetched in curently staying and bookings

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,16 +1,76 @@
 import React, { Component } from 'react'
 import Navigation from './Navigation';
+import { Table } from 'reactstrap'
+import { Container ,Button} from 'reactstrap'
+import Axios from 'axios';
 
 export default class Booking extends Component {
-    render() {
-        return(
-            <React.Fragment>
-            <div>
-            <Navigation/>
-                <h1>bookings</h1>
-            </div>
-            </React.Fragment>
 
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+
+      config: {
+        headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+    },
+       completeds: [],
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:3001/bookings/hotel/hotelbooking/completed',this.state.config)
+    .then((response)=>{
+      const data = response.data;
+      this.setState({completeds:  data});
+      console.log("data fecth");
+     
+    }).catch(error => console.log(error.response));
+  }
+
+ 
+
+ 
+  
+    render() {
+      console.log(this.state.popular)
+        return (
+            <React.Fragment>
+                <Navigation />
+                <Container className='mt-4'>
+            <h1> <p>Bookngs in our hotel </p></h1>
+            <Table id='featuretable'>
+                    <thead className="table-dark">
+                        <th>Guest</th>
+                        <th>Name</th>
+                        <th>CheckIn Date</th>
+                        <th>CheckOut Date</th>
+                        <th>Make CheckIn</th>
+                        <th>Delete</th>
+                    </thead>
+                    <tbody>
+                    {
+                            this.state.completeds.map((completed =>
+                        <tr>
+
+                            <td>
+                            <div className="image_wrap">
+                              <img src={`http://localhost:3001/uploads/${completed.guest.image}`}/>
+                            </div>
+                            </td>
+                            <td>{completed.guest.fullname}</td>
+                            <td>{completed.checkin}</td>
+                            <td>{completed.checkout}</td>
+                           <td><Button size='sm' color='warning' >CheckIn</Button></td> 
+                           <td> <Button size='sm' color='danger'>Delete</Button></td>
+                        </tr>
+                        ))
+                        }
+                    </tbody>
+                </Table>
+            </Container>
+            </React.Fragment>
+           
         )
     }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,74 @@
 import React, { Component } from 'react'
 import Navigation from './Navigation';
-
+import { Table } from 'reactstrap'
+import { Container ,Button} from 'reactstrap'
+import Axios from 'axios';
 
 export default class Dashboard extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+
+      config: {
+        headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+    },
+       completeds: [],
+    }
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:3001/bookings/hotel/hotelbooking/completed',this.state.config)
+    .then((response)=>{
+      const data = response.data;
+      this.setState({completeds:  data});
+      console.log("data fecth");
+     
+    }).catch(error => console.log(error.response));
+  }
+
+ 
+
+ 
+  
     render() {
+      console.log(this.state.popular)
         return (
             <React.Fragment>
-            <div>
-            <Navigation/>
-                <h1>checkedin</h1>
-            </div>
-            </React.Fragment>
+                <Navigation />
+                <Container className='mt-4'>
+            <h1> <p>Guests Staying </p></h1>
+            <Table id='featuretable'>
+                    <thead className="table-dark">
+                        <th>Guest</th>
+                        <th>Name</th>
+                        <th>CheckIn Date</th>
+                        <th>CheckOut Date</th>
+                        <th>Make CheckOut</th>
+                    </thead>
+                    <tbody>
+                    {
+                            this.state.completeds.map((completed =>
+                        <tr>
 
+                            <td>
+                            <div className="image_wrap">
+                              <img src={`http://localhost:3001/uploads/${completed.guest.image}`}/>
+                            </div>
+                            </td>
+                            <td>{completed.guest.fullname}</td>
+                            <td>{completed.checkin}</td>
+                            <td>{completed.checkout}</td>
+                           <td><Button size='sm' color='warning' >CheckOut</Button></td> 
+                        </tr>
+                        ))
+                        }
+                    </tbody>
+                </Table>
+            </Container>
+            </React.Fragment>
+           
         )
     }
 }
